40f504aad0758a0bdc81a85acceb4d5283caafab
Luiz Carraro <luiz.carraro@viasoft.com.br>
2017-01-19 14:27:39 -0200

    Add testes generated by ember-cli

---
+++ tests/unit/helpers/plusone-test.js
@@ -0,0 +1,12 @@
+
+import { plusone } from 'ember-example/helpers/plusone';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | plusone');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = plusone([42]);
+  assert.ok(result);
+});
+
